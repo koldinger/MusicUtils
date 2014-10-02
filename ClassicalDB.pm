f2705d5e3e05828aa70706b7a6a30f968c22344c
kolding <>
2014-10-02 19:52:49 +0000

    Various improvements that should have been checked in long ago.

--- ClassicalDB.pm
+++ ClassicalDB.pm
@@ -7,7 +7,8 @@ use Carp;
 use strict;
 
 # create a JSON object, just in case we want to use it more than once.
-my $json = new JSON();
+#my $json = new JSON();
+my $json = JSON->new->allow_nonref;
 
 sub parse {
     my $package = shift;
@@ -17,7 +18,7 @@ sub parse {
     my @lines = <JSONFILE>;
     close JSONFILE;
     my $line = join("", @lines);
-    my $obj = $json->jsonToObj($line);
+    my $obj = $json->decode($line);
     bless $obj;
     # print "Done parsing $file\n";
     return $obj;
@@ -28,38 +29,49 @@ sub getTrack {
     my $result = {};
     $trackNum = int ($trackNum);
     $set      = int ($set) if defined $set;
+
+	my $disc;
+
     my $works = $obj->{works};
     if (defined $works) {
         foreach my $work (@$works) {
-            if ((!defined($work->{SET}) && !defined $set) || (int($work->{SET}) == $set)) {
-                if ($work->{TRACKNUMBER} == $trackNum) {
-                    setTags ($obj,  $result);
-                    setTags ($work, $result);
-                    return $result;
-                }
-                my $tracks = $work->{tracks};
-                foreach my $track (@$tracks) {
-                    if ($track->{TRACKNUMBER} == $trackNum) {
-                        setTags ($obj,  $result);
-                        setTags ($work, $result);
-                        setTags ($track, $result);
-                        return $result;
-                    }
-                }
-            }
-        }
+			#print "WORK: $work->{TITLE} $work->{OPUS}\n";
+			$disc = $work->{SET};
+			$disc = $obj->{SET} unless defined $disc;
+
+			## This first one should never match.
+            if (($disc == $set) && ($work->{TRACKNUMBER} == $trackNum)) {
+				setTags ($obj,  $result);
+				setTags ($work, $result);
+				return $result;
+			}
+			my $tracks = $work->{tracks};
+			foreach my $track (@$tracks) {
+				#print "TRACK: $track\n";
+				$disc = $track->{SET} if defined($track->{SET});
+				$disc = int($disc) if defined $disc;
+				if (($disc == $set) && ($track->{TRACKNUMBER} == $trackNum)) {
+					setTags ($obj,  $result);
+					setTags ($work, $result);
+					setTags ($track, $result);
+					return $result;
+				}
+			}
+		}
     } 
     my $tracks = $obj->{tracks};
     if (defined $tracks) {
         foreach my $track (@$tracks) {
-            if ($track->{TRACKNUMBER} == $trackNum) {
-                if ((!defined($track->{SET}) && !defined $set) || (int($track->{SET}) == $set)) {
-                    setTags ($obj,  $result);
-                    setTags ($track, $result);
-                    return $result;
-                }
+			$disc = $track->{SET};
+			$disc = $obj->{SET} unless defined $disc;
+			$disc = int($disc) if defined $disc;
+			#print $disc, "**", $track->{TRACKNUMBER}, "\n";
+            if (($disc == $set) && ($track->{TRACKNUMBER} == $trackNum)) {
+				setTags ($obj,  $result);
+				setTags ($track, $result);
+				return $result;
             }
-	}
+		}
     }
     return undef;
 }
