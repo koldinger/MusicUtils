16a5bd0d2c385fbabe4aec608c2ca669e1ce9d66
Eric Koldinger <kolding@washington.edu>
2023-06-10 14:56:22 -0700

    Added copyright notice.   Cleanup of some processing.

--- converter.py
+++ converter.py
@@ -1,4 +1,33 @@
 #! /usr/bin/env python3
+# vim: set et sw=4 sts=4 fileencoding=utf-8:
+#
+# MusicUtilities: A set of utilities for working with music files.
+# Copyright 2013-2024, Eric Koldinger, All Rights Reserved.
+# kolding@washington.edu
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import pathlib
 import argparse
@@ -34,14 +63,14 @@ formats = {
     "audio/x-m4a": "mp4"
     }
 
-suffixes = {
-            "flac": ".flac",
-            "ipod": ".m4a",
-            "mp4" : ".mp4",
-            "mp3" : ".mp3",
-            "ogg" : ".ogg",
-            "alac": ".alac",
-            "ape": ".ape"
+inputtypes = {
+            ".flac": "flac",
+            ".m4a": "ipod",
+            ".mp4" : "mp4",
+            ".mp3" : "mp3",
+            ".ogg" : "ogg",
+            ".alac": "alac",
+            ".ape": "ape"
             }
 
 bitrates = {
@@ -50,10 +79,6 @@ bitrates = {
             "mp4": "128k"
            }
 
-#create a reverse map
-audiotypes = dict((v, k) for k, v in suffixes.items())
-
-suffix_values = suffixes.values()
 
 logger = None
 args = None
@@ -91,7 +116,7 @@ def collectFiles(src):
     for i in sorted(src.iterdir()):
         if i.is_dir():
             dirs.append(i)
-        elif i.suffix.lower() in suffix_values:
+        elif i.suffix.lower() in inputtypes:
             #files.append(i)
             yield i
 
@@ -120,15 +145,16 @@ def convert(job):
     dest = job.dest
     logger = job.logger
 
-    #print(f"Running job {src} ({src.suffix}) to {dest} ({dest.suffix}, {job.format}") 
+    print(f"Running job {src} ({src.suffix}) to {dest} ({dest.suffix}, {job.format}") 
 
     try:
         times = src.stat()
         tags = music_tag.load_file(src)
-        audio = AudioSegment.from_file(src, audiotypes[src.suffix])
+        audio = AudioSegment.from_file(src, inputtypes[src.suffix])
         logger.debug("Loaded %s", src)
     except Exception as e:
         logger.error("Failed loading %s: %s", src, e)
+        print(f"Failed loading {src} {e}")
         return f"{src} -> {dest} failed loading: {e}"
 
     logger.debug("Begining Conversion %s -> %s", src, dest)
@@ -143,6 +169,7 @@ def convert(job):
             tmp.close()
     except Exception as e:
         logger.error("Failed writing %s: %s", dest, e)
+        print(f"Failed writing {dest}: {e}")
         return f"{src} -> {dest} failed writing {e}"
 
     if args.copytags:
